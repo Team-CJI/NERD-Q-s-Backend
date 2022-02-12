3d25521cb0fa703067f85ff25eb698a2a1879c9c
Cedric Vallieu <cvallieu94@gmail.com>
2022-02-12 15:29:15 -0800

    ALL CRUD OPERATIONS FUNCTIONAL ON BACKEND

--- models/scoreModel.js
+++ models/scoreModel.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const scoreSchema = new Schema({
     "score": { type: String, required: true },
+    "email": { type: String, required: false },
 })
 
 const scoreModel = mongoose.model('scoreModel', scoreSchema);
--- server.js
+++ server.js
@@ -34,26 +34,26 @@ app.get('/', (request, response) => {
     response.send('test request recieved')
 })
 
-// app.get('/videogames', handleGetVideogames);
-app.get('/scores', handlePostScores);
-// app.get('/cars', handleGetCars);
-// app.get('/sports', handleGetSports);
 
-// const TriviaModel = require('./models/ex.js');
+app.post('/scores', handlePostScores);
+app.get('/scores', handleGetScores);
+app.delete('/scores/:id', handleDeleteScores);
+app.put('/scores/:id', handleUpdateScores);
+// app.get('/user', handleGetUser);
 
 
 const Data = { };
 
-class VideoGame {
-    constructor(videoGame) {
-        this.category = videoGame.category;
-        this.type = videoGame.type;
-        this.difficulty = videoGame.difficulty;
-        this.question = videoGame.question;
-        this.correct_answer = videoGame.correct_answer;
-        this.incorrect_answer = videoGame.incorrect_answer;
-    }
-}
+// class VideoGame {
+//     constructor(videoGame) {
+//         this.category = videoGame.category;
+//         this.type = videoGame.type;
+//         this.difficulty = videoGame.difficulty;
+//         this.question = videoGame.question;
+//         this.correct_answer = videoGame.correct_answer;
+//         this.incorrect_answer = videoGame.incorrect_answer;
+//     }
+// }
 
 async function handlePostScores(request, response) {
     // take score and pass to mongo
@@ -61,7 +61,8 @@ async function handlePostScores(request, response) {
     try {
         let queryObj = {};
         if (request.query.score) {
-            queryObj = { score: request.query.score }
+            queryObj = { score: request.query.score,
+            email: request.query.email }
             console.log(request.query.score)
         }
         let newScore = await scoreModel.create(request.query)
@@ -79,5 +80,154 @@ async function handlePostScores(request, response) {
         response.status(500).send('Sorry, but your score could not be added. WOMP WOMP');
     }
 }
+
+async function handleGetScores(request, response) {
+    // if (this.props.auth0.isAuthenticated) {
+    //   const res = await this.props.auth0.getIdTokenClaims();
+    //   const jwt = res.__raw;
+    //   console.log("jwt: ", jwt);
+    //   const config = {
+    //     headers: { "Authorization": `Bearer ${jwt}` },
+    //     method: 'get',
+    //     baseURL: process.env.REACT_APP_SERVER,
+    //     url: '/score'
+    //   }
+    // let apiUrl = `${SERVER}/books`;
+
+    // console.log(apiUrl);
+//     const HighScores = await mongoose.db.triva.find( {} );
+//     this.setState({ score: HighScores });
+//     console.log();
+//   }
+try {
+let queryObj = {};
+    if (request.query.email) {
+      queryObj = { email: request.query.email }
+    }
+    let scoresFromDB = await scoreModel.find((queryObj));
+
+    if (scoresFromDB) {
+      response.status(200).send(scoresFromDB);
+    } else {
+      response.status(404).send('no scores for you!');
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('server error');
+  }
+} 
+  
+
+
+  // async function handleDeleteScores(request, response) {
+  //   const id = request.params.id;
+    // const email = request.query.email;
+    //   try {
+    // console.log('score', score);
+    // const id = score._id;
+    // const email = score.email;
+    
+    // let newScore = this.state.score;
+    // console.log(newScore);
+    // newScore = this.state.score.filter(score => score._id !== id);
+    // this.setState({ score: newScore });
+    // console.log(id);
+    // const score = await scoreModel.findOne({ _id: id, email: email });
+    // if (!score) {
+    //     response.status(400).send('unable to delete score');
+    //     return;
+    //   }
+  
+    // //   if (score.email !== email) {
+    // //     response.status(400).send('unable to delete score');
+    // //     return;
+    // //   }
+  //     await scoreModel.findByIdAndDelete(id);
+  //     response.status(202).send('This score has been Removed!')
+  //   } catch (error) {
+  //     console.log(error);
+  //     response.status(404).send('unable to delete score');
+  //   }
+  // }
+
+  async function handleDeleteScores(request, response) {
+    const id = request.params.id;
+    const email = request.query.email;
+    try {
+      const score = await scoreModel.findOne({ _id: id, email: email });
+    console.log(score);
+    if (!score) {
+      response.status(400).send('unable to delete score');
+      return;
+    }
+
+    if (score.email !== email) {
+      response.status(400).send('unable to delete score');
+      return;
+    }
+      await scoreModel.findByIdAndDelete(id);
+      response.status(200).send('This score has been Removed!')
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('unable to delete score');
+    }
+
+  }
+    // const config = {
+    //   params: { email: this.props.user.email },
+    //   method: 'delete',
+    //   baseURL: `${SERVER}`,
+    //   url: `/score/${id}`
+    // }
+    // await axios(config);
+    // console.log(config);
+
+
+
+  async function handleUpdateScores(request, response) {
+    // console.log('score', updateScoreId);
+    // const id = updateScoreId._id;
+    // let updateScores = this.state.score;
+    // console.log(updateScores);
+    // updateScores = this.state.score.map(currentBook => currentBook._id === updateScoreId._id ? updateScores : currentBook );
+    // this.setState({ books: updateScores });
+    // console.log(id);
+    // const config = {
+    //   params: { email: this.props.user.email },
+    //   data: {score: updateScoreId.score},
+    //   method: 'put',
+    //   baseURL: `${SERVER}`,
+    //   url: `/score/${id}`
+    // }
+    // let res = await axios(config);
+    // console.log(res);
+
+  //   this.handleGetScores();
+  // }
+
+  try {
+    const id = request.params.id;
+    const email = request.query.email;
+
+    const scoreUpdate = await scoreModel.findOne({ _id: id, email: email })
+
+    if (!scoreUpdate) {
+      response.status(404).send('no scores for you!');
+      return;
+    }
+    if (scoreUpdate.email !== email) {
+      response.status(400).send('unable to update score');
+      return;
+    }
+    const updatedscore = await scoreModel.findByIdAndUpdate(id, request.body, { new: true});
+    response.send(updatedscore)
+
+  }
+  catch (error) {
+    console.error(error);
+    response.status(500).send('We have a problem');
+  }
+}
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
