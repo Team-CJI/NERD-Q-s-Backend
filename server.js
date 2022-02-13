0a1fd1a786ac0f6640a3255811a97c2002e50330
Cedric Vallieu <cvallieu94@gmail.com>
2022-02-12 16:49:51 -0800

    updated CRUD for frontend quiz completion

--- server.js
+++ server.js
@@ -44,29 +44,19 @@ app.put('/scores/:id', handleUpdateScores);
 
 const Data = { };
 
-// class VideoGame {
-//     constructor(videoGame) {
-//         this.category = videoGame.category;
-//         this.type = videoGame.type;
-//         this.difficulty = videoGame.difficulty;
-//         this.question = videoGame.question;
-//         this.correct_answer = videoGame.correct_answer;
-//         this.incorrect_answer = videoGame.incorrect_answer;
-//     }
-// }
-
-async function handlePostScores(request, response) {
+async function handlePostScores1(request, response) {
     // take score and pass to mongo
     
     try {
         let queryObj = {};
-        if (request.query.score) {
-            queryObj = { score: request.query.score,
+        console.log(request);
+        if (request.body.score) {
+            queryObj = { score: request.body.score,
             email: request.query.email }
-            console.log(request.query.score)
+            console.log(request.body.score)
         }
-        let newScore = await scoreModel.create(request.query)
-
+        let newScore = await scoreModel.create(request.body)
+        console.log(newScore);
 
         if (newScore) {
             response.status(201).send(newScore);
@@ -80,6 +70,33 @@ async function handlePostScores(request, response) {
         response.status(500).send('Sorry, but your score could not be added. WOMP WOMP');
     }
 }
+
+async function handlePostScores(request, response) {
+  // take score and pass to mongo
+  
+  try {
+      let queryObj = {};
+      if (request.query.score) {
+          queryObj = { score: request.query.score,
+          email: request.query.email }
+          console.log(typeof request.query.score)
+      }
+      let newScore = await scoreModel.create(queryObj)
+
+
+      if (newScore) {
+          response.status(201).send(newScore);
+      } else {
+          response.status(500).send("Sorry, but your score could not be added. WOMP WOMP");
+      }
+
+      
+  } catch (error) {
+      console.error(error);
+      response.status(500).send('Sorry, but your score could not be added. WOMP WOMP');
+  }
+}
+
 
 async function handleGetScores(request, response) {
     // if (this.props.auth0.isAuthenticated) {
@@ -116,39 +133,7 @@ let queryObj = {};
     response.status(500).send('server error');
   }
 } 
-  
 
-
-  // async function handleDeleteScores(request, response) {
-  //   const id = request.params.id;
-    // const email = request.query.email;
-    //   try {
-    // console.log('score', score);
-    // const id = score._id;
-    // const email = score.email;
-    
-    // let newScore = this.state.score;
-    // console.log(newScore);
-    // newScore = this.state.score.filter(score => score._id !== id);
-    // this.setState({ score: newScore });
-    // console.log(id);
-    // const score = await scoreModel.findOne({ _id: id, email: email });
-    // if (!score) {
-    //     response.status(400).send('unable to delete score');
-    //     return;
-    //   }
-  
-    // //   if (score.email !== email) {
-    // //     response.status(400).send('unable to delete score');
-    // //     return;
-    // //   }
-  //     await scoreModel.findByIdAndDelete(id);
-  //     response.status(202).send('This score has been Removed!')
-  //   } catch (error) {
-  //     console.log(error);
-  //     response.status(404).send('unable to delete score');
-  //   }
-  // }
 
   async function handleDeleteScores(request, response) {
     const id = request.params.id;
